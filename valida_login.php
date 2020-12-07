e23befb9991db6e3f7ef4b4b97bd559329aeb039
Joelson Ramos Ferreira <47796454+joelsonRamos@users.noreply.github.com>
2020-12-07 15:28:43 -0300

    dev

---
+++ valida_login.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+// variavel que verifica a autenticação foi realizada
+$usuario_autenticado = false;
+
+
+// usuarios do sistema
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' =>'123456'),
+    array('email' => 'user@teste.com.br', 'senha' =>'abcd'),
+);
+
+foreach($usuarios_app as $user){
+
+    if($user['email'] == $_POST['email']  && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+}
+if($usuario_autenticado){
+    echo 'usuario autenticado';
+    $_SESSION['autenticado'] = 'SIM';
+    header('Location: home.php');
+}else{
+    $_SESSION['autenticado'] = 'NAO';
+    header('Location: index.php?login=erro');
+}
+// echo '<pre>';
+// print_r($usuarios_app);
+// echo '</pre>';
+// print_r($_GET);
+// echo '<br>';
+//     echo $_GET['email'];
+//     echo '<br>';
+//     echo $_GET['senha'];
+// print_r($_POST);
+// echo '<br>';
+// echo $_POST['email'];
+// echo '<br>';
+// echo $_POST['senha'];
+
+?>
